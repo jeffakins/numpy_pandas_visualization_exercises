8a67c1ef24f145e777dd279b96a9aea698335207
Jeff Akins <jeffakins21@gmail.com>
2021-07-15 12:41:10 -0700

    NumPy exercise work

---
+++ numpy_exercises.py
@@ -0,0 +1,11 @@
+# NumPy Exercises
+
+import numpy as np
+
+a = np.array([4, 10, 12, 23, -2, -1, 0, 0, 0, -6, 3, -7])
+
+# Questions for the above array
+
+# 1. How many negative numbers are there?
+
+print(a < 0)
