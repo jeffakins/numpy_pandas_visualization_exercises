f181ab532b90df4b914f0ad397d13c3c90c84ac7
Jeff Akins <jeffakins21@gmail.com>
2021-07-16 13:22:43 -0700

    Start of exercise 3 for Pandas

--- pandas_series.py
+++ pandas_series.py
@@ -42,7 +42,42 @@ fruit_series.value_counts().head(1)                     # Kiwi
 # 10. Determine the string value that occurs least frequently in fruits.
 fruit_series.value_counts().nsmallest(n=1, keep="all")                             
 
-#
+#--------------------------------------------------------------------------
+
+# Exercises Part II
+
+#1. Capitalize all the string values in fruits.
+
+fruit_series.to_string().capitalize()
+
+# 2. Count the letter "a" in all the string values (use string vectorization).
+
+# 3. Output the number of vowels in each and every string value.
+
+# 4. Write the code to get the longest string value from fruits.
+max(fruit_series, key=len)
+
+# 5. Write the code to get the string values with 5 or more letters in the name.
+
+# 6. Use the .apply method with a lambda function to find the fruit(s) containing the letter "o" two or more times.
+
+# 7. Write the code to get only the string values containing the substring "berry".
+
+# 8. Write the code to get only the string values containing the substring "apple".
+
+# 9. Which string value contains the most vowels?
 
 
+#--------------------------------------------------------------------------
+
+# Exercises Part III
+import pandas as pd
+letters = pd.Series(['hnvidduckkqxwymbimkccexbkmqygkxoyndmcxnwqarhyffsjpsrabtjzsypmzadfavyrnndndvswreauxovncxtwzpwejilzjrmmbbgbyxvjtewqthafnbkqplarokkyydtubbmnexoypulzwfhqvckdpqtpoppzqrmcvhhpwgjwupgzhiofohawytlsiyecuproguy'])
+letters
+
+# 1. Which letter occurs the most frequently in the letters Series?
+split_letters = letters.str.split(pat="", n=0, expand=True)         # Splits the string into individual letters (on one row tho) 
+transpose_letters = split_letters.transpose()                       # Turns the row into a column
+transpose_letters.value_counts()                                    # Groups the letters by number of occurence
+transpose_letters.value_counts().head(1)                            # Selects the letter that occured the most frequesntly: y; 13 times
 
